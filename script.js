3e71d01a8fbc8a66efedbab01c254746f189dfa3
Kseniya <bovda.kseniya@gmail.com>
2023-02-20 00:05:34 +0600

    Добавлены задания с JS

---
+++ script.js
@@ -0,0 +1,75 @@
+// Задание 1
+let a = 10;
+alert(a);
+/*
+	let a = 20;
+    alert(a);
+*/
+
+
+// Задание 2 
+let yearOfReleaseOfTheFirstIphone = 2007;
+alert(yearOfReleaseOfTheFirstIphone);
+
+
+// Задание 3
+let nameOfTheCreatorOfTheJavaScriptLanguage = `Brendan Eich`;
+alert(nameOfTheCreatorOfTheJavaScriptLanguage);
+
+
+// Задание 4 
+let number1 = 10;
+let number2 = 2;
+alert(number1+number2);
+alert(number1-number2);
+alert(number1*number2);
+alert(number1/number2);
+
+// Задание 6
+let number = 2;
+result = number ** 5;
+alert(result)
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+// Задача 8
+let age = Number(prompt(`Сколько вам лет?`));
+alert(age)
+
+
+// Задание 9.0
+const user = {
+    name: `Kseniya`,
+    age: 22,
+    isAdmin: true,
+}
+
+
+// Задание 9.1
+user [`city of residence`] = `Omsk`;
+
+
+// Задание 9.2
+user.age = surname;
+
+
+// Задание 9.3
+delete [`city of residence`];
+
+
+// Задание 9.4
+let info = prompt(`Что вы хотите узнать о пользователе?`);
+alert(user[info]);
+
+
+// Задание 10
+let question = String(prompt(`Как вас зовут?`));
+alert(`Привет ${question} "!""`);
