b395aea0b188c1d531272c65c1e692fca35064b9
Kseniya <bovda.kseniya@gmail.com>
2023-02-22 07:24:48 +0600

    исправила ошибки

--- script-2.js
+++ script-2.js
@@ -3,7 +3,7 @@
 // Задание 1
 
 let password = 'пароль';
-let question = prompt(`Введите пароль`);
+let questionPassword = prompt(`Введите пароль`);
 
 if (password == true) {
     alert(`Пароль введен верно`);
@@ -14,25 +14,84 @@ if (password == true) {
 // Задание 2
 
 let c = 5;
-let messege;
 
 if (c > 0 && c < 10) {
-    messege = `Верно`;
+    alert(`Верно`);
 } else {
-    messege = `Неверно`;
+    alert(`Неверно`);
 }
 
 // Задание 3
 
+let d = 10;
+let e = 1000;
+
+if (d > 100 || e > 100) {
+    alert(`Верно`)
+} else {
+    alert(`Неверно`)
+}
+
 // Задание 4
+
+let a = Number(2);
+let b = Number(3);
+alert(a + b);
 
 // Задание 5
 
-// Задание 6
+let monthNumber = prompt(`Введите номер месяца`)
+
+switch (monthNumber) {
+    case `1`:
+        alert(`зима`)
+        break;
+    case `2`:
+        alert(`зима`)
+        break;
+    case `3`:
+        alert(`весна`)
+        break;
+    case `4`:
+        alert(`весна`)
+        break;
+    case `5`:
+        alert(`весна`)
+        break;
+    case `6`:
+        alert(`лето`)
+        break;
+    case `7`:
+        alert(`лето`)
+        break;
+    case `8`:
+        alert(`лето`)
+        break;
+    case `9`:
+        alert(`осень`)
+        break;
+    case `10`:
+        alert(`осень`)
+        break;
+    case `11`:
+        alert(`осень`)
+        break;
+    case `12`:
+        alert(`зима`)
+        break;
+
+    default:
+        alert(`Такого месяца не существует!`)
+        break;
+}
 
 // Задание 7
 
+
+
 // Задание 8
+ 
+
 
 // Задание 9
 
